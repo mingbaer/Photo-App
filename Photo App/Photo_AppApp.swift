9558f5711af3a290299246977f16853fc9d517a5
Alexandra Baer Chan <mingbaer@gmail.com>
2024-05-12 20:04:50 -0500

    Initial Commit

---
+++ Photo App/Photo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_AppApp.swift
+//  Photo App
+//
+//  Created by Alexandra Baer Chan on 5/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
